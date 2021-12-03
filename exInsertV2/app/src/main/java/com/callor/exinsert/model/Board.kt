01c2cae3524222bf0a4ab8c36a03633ab320d2a3
kimyoungjin98 <gyu250@naver.com>
2021-12-03 17:41:51 +0900

    추가

---
+++ exInsertV2/app/src/main/java/com/callor/exinsert/model/Board.kt
@@ -0,0 +1,9 @@
+package com.callor.exinsert.model
+
+data class Board(
+    var seq:Int,
+    var author:String,
+    var title:String,
+    var date:String,
+    var content:String,
+)
--- exInsertV2/app/src/main/java/com/callor/exinsert/ui/dashboard/DashboardFragment.kt
+++ exInsertV2/app/src/main/java/com/callor/exinsert/ui/dashboard/DashboardFragment.kt
@@ -1,22 +1,40 @@
 package com.callor.exinsert.ui.dashboard
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.callor.exinsert.MainActivity
 import com.callor.exinsert.R
 import com.callor.exinsert.databinding.FragmentDashboardBinding
+import com.callor.exinsert.model.Board
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
     private var _binding: FragmentDashboardBinding? = null
 
+    lateinit var mainActivity: MainActivity
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    // context로 설정해서 Toast 띄우기
+    // 프로젝트 자체에선 필요x 코드
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mainActivity = context as MainActivity
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +46,17 @@ class DashboardFragment : Fragment() {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
-        var c_author = binding.cAuthor
+        var send:Button = binding.btnSend
+        send.setOnClickListener(View.OnClickListener {
+
+           var author:EditText = binding.author
+           var title:EditText = binding.title
+           var date:EditText = binding.date
+           var content:EditText = binding.content
+
+            Log.d("title",  )
+
+        })
 
 
 
